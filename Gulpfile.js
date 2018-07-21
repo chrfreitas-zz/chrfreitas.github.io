6660cf04e0e0ff0b2e7f056276a370d6a5e7afbc
Christian Freitas <chrfreitas@gmail.com>
2018-07-21 14:20:31 -0300

    Create new dev environment

--- Gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var path = require('path');
-
-gulp.task('less', function () {
-    gulp.src('app/less/*')
-        .pipe(less())
-        .pipe(gulp.dest('app/css/'));
-});
